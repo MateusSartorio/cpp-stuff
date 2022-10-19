6c88ad3f66e9746b66dacf458bab729acb3db341
Mateus <mateussartorio39@gmail.com>
2022-10-18 21:58:31 -0300

    28-10-2022

--- vector/client.cpp
+++ vector/client.cpp
@@ -2,7 +2,10 @@
 #include "vector.h"
 
 int main(int argc, char** argv) {
-    
+    vector<std::string> vec;
+    vec.push_back("ola");
+
+    std::cout << vec[0] << std::endl;
 
     return 0;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -5,15 +5,15 @@
 template<typename T>
 class vector {
     private:
-        T* data = nullptr
+        T* data = nullptr;
         size_t size = 0;
         size_t capacity = 2;
 
         void realloc(size_t new_capacity) {
             T* new_block = new T[new_capacity];
 
-            size_t size = this->size;
-            
+            if(new_capacity < size)
+                this->size = new_capacity;
 
             for(size_t i = 0; i < this->size; i++)
                 new_block[i] = this->data[i];
@@ -26,5 +26,28 @@ class vector {
     public:
         vector() {
             realloc(this->capacity);
+        }
+
+        void push_back(const T& value) {
+            if(this->size >= this->capacity)
+                realloc((3/2)*this->size);
+
+            this->data[this->size++] = value;
+        }
+
+        size_t get_size() const {
+            return this->size;
+        }
+
+        size_t get_size() {
+            return this->size;
+        }
+
+        T& operator[](size_t index) const {
+            return this->data[index];
+        }
+
+        T& operator[](size_t index) {
+            return this->data[index];
         }
 };
